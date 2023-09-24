ba51b7f0edae6accc739a54bbc6939b4c496cbfd
Daniel Tsuzuki <massao.tsuzuki@gmail..com>
2023-09-24 17:41:23 -0300

    Criando DTO para requisicao salvarCliente

---
+++ msclientes/src/main/java/com/github/danieltsuzuki/msclientes/application/representation/ClienteSaveRequest.java
@@ -0,0 +1,16 @@
+package com.github.danieltsuzuki.msclientes.application.representation;
+
+import com.github.danieltsuzuki.msclientes.domain.Cliente;
+import lombok.Data;
+
+@Data
+public class ClienteSaveRequest {
+
+    private String cpf;
+    private String nome;
+    private Integer idade;
+
+    public Cliente toModel(){
+        return new Cliente(cpf, nome, idade);
+    }
+}
