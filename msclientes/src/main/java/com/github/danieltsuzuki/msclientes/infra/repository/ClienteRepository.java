33d9bea364e9adfa4b8b64b6b2ae67117d7650ab
Daniel Tsuzuki <massao.tsuzuki@gmail..com>
2023-09-24 17:40:41 -0300

    Corrigindo metodo findyCpf

--- msclientes/src/main/java/com/github/danieltsuzuki/msclientes/infra/repository/ClienteRepository.java
+++ msclientes/src/main/java/com/github/danieltsuzuki/msclientes/infra/repository/ClienteRepository.java
@@ -8,5 +8,5 @@ import java.util.Optional;
 
 @Repository
 public interface ClienteRepository extends JpaRepository<Cliente, Long> {
-    Optional<Cliente> findByCPF(String cpf);
+    Optional<Cliente> findByCpf(String cpf);
 }
