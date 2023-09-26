9473c5d47c7f3fdd760c6a5aeb41fc3a4306a52f
Daniel Tsuzuki <massao.tsuzuki@gmail..com>
2023-09-26 20:53:58 -0300

    Criando rota para cartoesPorCliente no micro serviço mscartoes

--- mscartoes/src/main/java/com/github/danieltsuzuki/mscartoes/application/CartoesResource.java
+++ mscartoes/src/main/java/com/github/danieltsuzuki/mscartoes/application/CartoesResource.java
@@ -1,6 +1,7 @@
 package com.github.danieltsuzuki.mscartoes.application;
 
 import com.github.danieltsuzuki.mscartoes.application.representation.CartaoSaveRequest;
+import com.github.danieltsuzuki.mscartoes.application.representation.CartoesPorClienteResponse;
 import com.github.danieltsuzuki.mscartoes.domain.Cartao;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
@@ -8,13 +9,15 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
+import java.util.stream.Collectors;
 
 @RestController
 @RequestMapping("cartoes")
 @RequiredArgsConstructor
 public class CartoesResource {
 
-    private final CartaoService service;
+    private final CartaoService cartaoService;
+    private final ClienteCartaoService clienteCartaoService;
 
     @GetMapping
     public String status(){
@@ -24,14 +27,22 @@ public class CartoesResource {
     @PostMapping
     public ResponseEntity cadastra(@RequestBody CartaoSaveRequest request){
         var cartao = request.toModel();
-        service.save(cartao);
+        cartaoService.save(cartao);
         return ResponseEntity.status(HttpStatus.CREATED).build();
     }
 
     @GetMapping(params = "renda")
     public ResponseEntity<List<Cartao>> getCartoesRendaAteh(@RequestParam("renda") Long renda){
-        var listaCartoes = service.getCartoesRendaMenorIgual(renda);
+        var listaCartoes = cartaoService.getCartoesRendaMenorIgual(renda);
         return ResponseEntity.ok(listaCartoes);
+    }
+
+    @GetMapping(params = "cpf")
+    public ResponseEntity<List<CartoesPorClienteResponse>> getCartoesByCliente(@RequestParam("cpf") String cpf){
+        var listaCartoesCliente = clienteCartaoService.listCartoesByCpf(cpf)
+                .stream().map(CartoesPorClienteResponse::fromModel).collect(Collectors.toList());
+
+        return ResponseEntity.ok(listaCartoesCliente);
     }
 
 }
