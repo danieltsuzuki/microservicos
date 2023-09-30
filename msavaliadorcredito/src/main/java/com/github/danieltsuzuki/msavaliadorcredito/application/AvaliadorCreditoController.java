721ab003aa57165229630ad9803cbf8f7f800217
Daniel Tsuzuki <massao.tsuzuki@gmail..com>
2023-09-30 00:34:16 -0300

    Finalizando a implementacao do endpoint consultaSituacao, adicionando tratamento de erros a comunicacao sincrona entre os servicos

--- msavaliadorcredito/src/main/java/com/github/danieltsuzuki/msavaliadorcredito/application/AvaliadorCreditoController.java
+++ msavaliadorcredito/src/main/java/com/github/danieltsuzuki/msavaliadorcredito/application/AvaliadorCreditoController.java
@@ -1,12 +1,13 @@
 package com.github.danieltsuzuki.msavaliadorcredito.application;
 
+import com.github.danieltsuzuki.msavaliadorcredito.application.ex.DadosClienteNotFoundException;
+import com.github.danieltsuzuki.msavaliadorcredito.application.ex.ErroComunicacaoMicroservicesException;
+import com.github.danieltsuzuki.msavaliadorcredito.domain.model.DadosAvaliacao;
 import com.github.danieltsuzuki.msavaliadorcredito.domain.model.SituacaoCliente;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("avaliacoes-credito")
@@ -20,9 +21,18 @@ public class AvaliadorCreditoController {
         return "ok";
     }
     @GetMapping(value = "situacao-cliente", params = "cpf")
-    public ResponseEntity<SituacaoCliente> consultaSSituacaoCliente(@RequestParam("cpf") String cpf){
-        SituacaoCliente situacaoCliente = avaliadorCreditoService.obterSituacaoCliente(cpf);
+    public ResponseEntity consultaSituacaoCliente(@RequestParam("cpf") String cpf){
+        SituacaoCliente situacaoCliente = null;
+        try {
+            situacaoCliente = avaliadorCreditoService.obterSituacaoCliente(cpf);
         return ResponseEntity.ok(situacaoCliente);
+        } catch (DadosClienteNotFoundException e) {
+            return ResponseEntity.notFound().build();
+        } catch (ErroComunicacaoMicroservicesException e) {
+            return ResponseEntity.status(HttpStatus.resolve(e.getStatus())).body(e.getMessage());
+        }
     }
+
+
 
 }
--- msavaliadorcredito/src/main/java/com/github/danieltsuzuki/msavaliadorcredito/application/AvaliadorCreditoService.java
+++ msavaliadorcredito/src/main/java/com/github/danieltsuzuki/msavaliadorcredito/application/AvaliadorCreditoService.java
@@ -1,11 +1,15 @@
 package com.github.danieltsuzuki.msavaliadorcredito.application;
 
+import com.github.danieltsuzuki.msavaliadorcredito.application.ex.DadosClienteNotFoundException;
+import com.github.danieltsuzuki.msavaliadorcredito.application.ex.ErroComunicacaoMicroservicesException;
 import com.github.danieltsuzuki.msavaliadorcredito.domain.model.CartaoCliente;
 import com.github.danieltsuzuki.msavaliadorcredito.domain.model.DadosCliente;
 import com.github.danieltsuzuki.msavaliadorcredito.domain.model.SituacaoCliente;
 import com.github.danieltsuzuki.msavaliadorcredito.infra.clients.CartoesResourceClient;
 import com.github.danieltsuzuki.msavaliadorcredito.infra.clients.ClienteResourceClient;
+import feign.FeignException;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
 
@@ -18,15 +22,23 @@ public class AvaliadorCreditoService {
     private final ClienteResourceClient clientesClient;
     private final CartoesResourceClient cartoesClient;
 
-    public SituacaoCliente obterSituacaoCliente(String cpf){
-        ResponseEntity<DadosCliente> dadosClienteResponse = clientesClient.dadosCliente(cpf);
-        ResponseEntity<List<CartaoCliente>> cartoesResponse = cartoesClient.getCartoesByCliente(cpf);
+    public SituacaoCliente obterSituacaoCliente(String cpf) throws DadosClienteNotFoundException, ErroComunicacaoMicroservicesException {
+        try{
+            ResponseEntity<DadosCliente> dadosClienteResponse = clientesClient.dadosCliente(cpf);
+            ResponseEntity<List<CartaoCliente>> cartoesResponse = cartoesClient.getCartoesByCliente(cpf);
 
-        return SituacaoCliente
-                .builder()
-                .cliente(dadosClienteResponse.getBody())
-                .cartoes(cartoesResponse.getBody())
-                .build();
+            return SituacaoCliente
+                    .builder()
+                    .cliente(dadosClienteResponse.getBody())
+                    .cartoes(cartoesResponse.getBody())
+                    .build();
+        } catch (FeignException.FeignClientException e){
+            int status = e.status();
+            if (HttpStatus.NOT_FOUND.value() == status){
+                throw new DadosClienteNotFoundException();
+            }
+            throw new ErroComunicacaoMicroservicesException(e.getMessage(), status);
+        }
     }
 
 }
