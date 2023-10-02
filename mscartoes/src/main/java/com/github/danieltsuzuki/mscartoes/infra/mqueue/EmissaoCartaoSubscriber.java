7a92f604678ce58b82ee2a518cf92ecc5dab7c87
Daniel Tsuzuki <massao.tsuzuki@gmail..com>
2023-10-01 21:02:12 -0300

    Implementando a emissao de cartao via subscriber

--- mscartoes/src/main/java/com/github/danieltsuzuki/mscartoes/infra/mqueue/EmissaoCartaoSubscriber.java
+++ mscartoes/src/main/java/com/github/danieltsuzuki/mscartoes/infra/mqueue/EmissaoCartaoSubscriber.java
@@ -1,14 +1,40 @@
 package com.github.danieltsuzuki.mscartoes.infra.mqueue;
 
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.github.danieltsuzuki.mscartoes.domain.Cartao;
+import com.github.danieltsuzuki.mscartoes.domain.ClienteCartao;
+import com.github.danieltsuzuki.mscartoes.domain.DadosSolicitacaoEmissaoCartao;
+import com.github.danieltsuzuki.mscartoes.infra.repository.CartaoRepository;
+import com.github.danieltsuzuki.mscartoes.infra.repository.ClienteCartaoRepository;
+import lombok.RequiredArgsConstructor;
 import org.springframework.amqp.rabbit.annotation.RabbitListener;
 import org.springframework.messaging.handler.annotation.Payload;
 import org.springframework.stereotype.Component;
 
 @Component
+@RequiredArgsConstructor
 public class EmissaoCartaoSubscriber {
+
+    private final CartaoRepository CartaoRepository;
+    private final ClienteCartaoRepository clienteCartaoRepository;
+
     @RabbitListener(queues = "${mq.queues.emissao-cartoes}")
     public void receberSolicitacaoEmissao(@Payload String payload){
-        System.out.println(payload);
+        var mapper = new ObjectMapper();
+        try {
+            DadosSolicitacaoEmissaoCartao dados = mapper.readValue(payload, DadosSolicitacaoEmissaoCartao.class);
+            Cartao cartao = CartaoRepository.findById(dados.getIdCartao()).orElseThrow();
+
+            ClienteCartao clienteCartao = new ClienteCartao();
+            clienteCartao.setCartao(cartao);
+            clienteCartao.setCpf(dados.getCpf());
+            clienteCartao.setLimite(dados.getLimiteLiberado());
+
+            clienteCartaoRepository.save(clienteCartao);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
     }
 
 }
